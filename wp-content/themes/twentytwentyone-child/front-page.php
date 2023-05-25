3727dc5d2a39fca93e586d5b303b62df7f50df30
antikkorps <franckvienot7@gmail.com>
2023-05-25 18:20:31 +0200

    changed one selector with multiple pseudo-class

--- wp-content/themes/twentytwentyone-child/front-page.php
+++ wp-content/themes/twentytwentyone-child/front-page.php
@@ -439,7 +439,11 @@ $description = get_the_archive_description();
     </div>
 
     <!-- BACK TO TOP -->
-    <a href="#" class="backtotop d-flex justify-content-center align-items-center rounded-circle">top<i class="fas fa-arrow-up"></i></a>
+    <a href="#" class="backtotop d-flex justify-content-center align-items-center rounded-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
+        </svg>
+    </a>
 
     <!-- END CONTAINER PAGE -->
 </div>
